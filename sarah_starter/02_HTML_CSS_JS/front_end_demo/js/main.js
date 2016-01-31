8b687676318fff18b6045a5da8a9da4859aee98b
sarahholden <sarahbeth.holden@gmail.com>
2016-01-31 00:30:06 -0800

    Switched it up

--- sarah_starter/02_HTML_CSS_JS/front_end_demo/js/main.js
+++
@@ -1,9 +0,0 @@
-$('.change-color').click(function () {
-	$('body').css({
-		"background-color": "blue"
-	});
-});
-
-$('.alert').click(function () {
-	alert('Hello World!');
-});
---
+++ starter_code/variables/js/main.js
@@ -0,0 +1,23 @@
+// ----------------------------------
+// Part 1 - We do
+// ----------------------------------
+
+// 1. Declare a variable with the name "score". Assign it the value 3.
+
+// 2. Update the value of score. The new value should be 6.
+
+// 3. Use jQuery to update the html of #item1 with the value of score (use the .html() method)
+
+
+
+// ----------------------------------
+// Part 2 - You do
+// ----------------------------------
+
+// 1. Declare a variable with the name "totalAmount". Assign it the value 0.
+
+// 2. Update the value of totalAmount. The new value should be 10.
+
+// 3. Add five to the totalAmount using the += operator
+
+// 4. Use jQuery to update the html of #item2 with the value of totalAmount (use the .html() method)
